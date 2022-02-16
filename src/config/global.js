a8b4fab2a0f04a3086e73fcbebab34a08e8ccbc6
deivyfonseca <deivyfonseca@gmail.com>
2022-02-15 21:58:26 -0500

    Avance 01
    Se adelanta el banner principal, la introducción, el glosario, las referencias y la navegación

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
+    componenteFormativo: 'Aspectos legales y presupuestos',
+    descripcionCurso:
+      'El mercado exige una gran especialización e invita a ser experto en todas las áreas comprometidas de una labor. En un mundo en el que constantemente suceden cambios, es necesario estar preparado para afrontar la vida laboral desde la independencia y saber cómo conseguir, gestionar y comercializar proyectos a través de conocimientos financieros para definir si  “cuadran las cuentas”.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
   },
   menuPrincipal: {
@@ -18,34 +19,68 @@ export default {
         titulo: 'Introducción',
         desarrolloContenidos: true,
       },
+
       {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Legislación para industrias creativas y culturales',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            icono: 'far fa-file-alt',
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Formulación de proyectos',
         desarrolloContenidos: true,
       },
+
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: '¿Cómo realizar proyectos?',
+        desarrolloContenidos: true,
+      },
+
+      {
+        nombreRuta: 'tema4',
+        icono: 'far fa-file-alt',
+        numero: '4',
+        titulo: 'Presentar proyectos a convocatorias y concursos',
+        desarrolloContenidos: true,
+      },
+
+      {
+        nombreRuta: 'tema5',
+        icono: 'far fa-file-alt',
+        numero: '5',
+        titulo: 'Fuentes de financiación para proyectos de animación digital',
+        desarrolloContenidos: true,
+      },
+
+      {
+        nombreRuta: 'tema6',
+        icono: 'far fa-file-alt',
+        numero: '6',
+        titulo: 'Seguimiento o monitoreo de proyectos',
+        desarrolloContenidos: true,
+      },
+
+      {
+        nombreRuta: 'tema7',
+        icono: 'far fa-file-alt',
+        numero: '7',
+        titulo: 'Modelo de negocio, costeo y presupuesto',
+        desarrolloContenidos: true,
+      },
+
+      {
+        nombreRuta: 'tema8',
+        icono: 'far fa-file-alt',
+        numero: '8',
+        titulo: 'Marketing digital',
         desarrolloContenidos: true,
       },
     ],
@@ -91,19 +126,100 @@ export default {
   referencias: [
     {
       referencia:
-        'Zlackman, C. (2012). 100 años de moda. Sistema de Bibliotecas SENA:',
+        'Departamento Administrativo de la Función Pública (2012). Guía para la construcción de indicadores de gestión:',
       link:
-        'https://sena-primo.hosted.exlibrisgroup.com/primo-explore/fulldisplay?docid=sena_aleph000060086&vid=SENA&search_scope=sena_completo&tab=sena_completo&lang=es_ES&context=L',
+        'https://www.funcionpublica.gov.co/documents/418537/506911/1595.pdf/6c897f03-9b26-4e10-85a7-789c9e54f5a3',
+    },
+    {
+      referencia:
+        'Fuentes Rueda, P (2020). Claves fundamentales para hacer un buen pitching audiovisual | UNIR:',
+      link:
+        'https://www.unir.net/humanidades/revista/claves-fundamentales-para-hacer-un-buen-pitching-audiovisual',
+    },
+    {
+      referencia:
+        'Lasala, C (2009) Formulación de proyectos audiovisuales. Mediax, gente de medios:',
+      link: 'https://www.mediax.online/',
+    },
+    {
+      referencia:
+        'Mendezu, J. (2020). Plantilla de presupuesto para animación 2D. Crehana:',
+      link:
+        'https://www.crehana.com/recursos/herramientas/presupuesto-animacion-2d',
+    },
+    {
+      referencia:
+        'Orlando, M; Ozollo, MF, (2005). Formulación de proyectos culturales. Universidad Nacional de Cuyo. Departamento de Educación a Distancia; Ediciones Biblioteca Digital UNCuyo:',
+      link: 'https://bdigital.uncu.edu.ar/1098',
+    },
+    {
+      referencia:
+        'Ortegon, E, Pacheco J. F. y Roura Horacio (2005). Metodología general de identificación, preparación y evaluación de proyectos de inversión pública. Instituto Latinoamericano de Planificación Económica y Social (ILPES).:',
+      link:
+        'https://www.cepal.org/es/publicaciones/5608-metodologia-general-identificacion-preparacion-evaluacion-proyectos-inversion',
+    },
+    {
+      referencia:
+        'Rosello, D. (2007). Diseños y evaluación de proyectos culturales. De la idea a la acción:',
+      link:
+        'https://proyectosculturalesundav.files.wordpress.com/2015/03/159034552-rosello-cerezuela-david-diseno-y-evaluacion-de-proyectos-culturales.pdf',
+    },
+    {
+      referencia:
+        'Secretaría de cultura, recreación y deporte de la Alcaldía Mayor de Bogotá (2020). Curso de formulación de proyectos culturales 2020:',
+      link: 'https://formacion.scrd.gov.co/',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Crowfunding',
+      significado:
+        'Cooperación colectiva para recaudar dinero por medio de las redes, con la finalidad de financiar un proyecto a personas como a colectivos.',
     },
     {
-      termino: '<em>Término</em>',
-      significado: 'Definición',
+      termino: 'Convocatoria',
+      significado:
+        'Es un llamado a concursar en un evento que puede ser público o privado y que ofrece algún beneficio para quien resulte favorecido. El beneficio puede ser de tipo económico, de tipo formativo (becas de estudio), consultoría (mentoring), participación, etc.',
+    },
+    {
+      termino: 'Costos',
+      significado:
+        'Pueden diferenciarse costos directos, indirectos, relacionados con el capital, fijos y variables.',
+    },
+    {
+      termino: 'Formulación de proyectos',
+      significado:
+        'Tiene que ver con las herramientas (metodologías, formatos) con los que se definen las acciones que se van a realizar. Es la manera como se interlocuta con el financiador/inversor.',
+    },
+    {
+      termino: 'Indicadores',
+      significado:
+        'Permiten evidenciar el nivel de cumplimiento acerca de lo que está haciendo la organización y sobre los efectos de sus actividades a través de la medición y seguimiento de aspectos definidos previamente.',
+    },
+    {
+      termino: 'Marketing digital',
+      significado:
+        'Se refiere a las acciones que una organización realiza para alcanzar unos objetivos determinados, que siempre están relacionados con el mejoramiento de los ingresos.',
+    },
+    {
+      termino: 'Metodología',
+      significado:
+        'Se refiere al método que se utiliza para estructurar la manera de formular el proyecto, (por ejemplo, a partir de los objetivos, línea base) y también a la manera como se gestionan los proyectos.',
+    },
+    {
+      termino: 'Presupuesto',
+      significado:
+        'Se trata de la organización y estimación de los recursos necesarios en cada una de las etapas y actividades del proyecto, divididos en tipos de gastos o rubros.',
+    },
+    {
+      termino: 'Proyecto',
+      significado:
+        'Se trata de una planificación que contempla un conjunto de actividades a realizar de manera articulada entre sí, con el fin de producir determinados bienes o servicios capaces de satisfacer necesidades o resolver problemas, dentro de los límites de un presupuesto y de un periodo de tiempo dados.',
+    },
+    {
+      termino: 'Social media',
+      significado:
+        'Son herramientas que se ponen en marcha para abrir diversos canales de comunicación en el mundo digital, que a la vez se constituyen como un canal de comunicación con una audiencia completamente digital.',
     },
   ],
   complementario: [
--- src/config/titulo.js
+++ src/config/titulo.js
@@ -1,1 +1,1 @@
-module.exports = 'NOMBRE DEL COMPONENTE FORMATIVO'
+module.exports = 'Aspectos legales y presupuestos'
